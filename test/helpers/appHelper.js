cc99b3ee005d45db8bf90a8582cf7f865ff185cc
Alexandre Tiertant <alexandre.tiertant@gmail.com>
2016-12-06 14:50:18 +0100

    Update appHelper.js

--- test/helpers/appHelper.js
+++ test/helpers/appHelper.js
@@ -237,9 +237,6 @@ module.exports = {
   linkDeps: function(appPath) {
     var deps = ['sails-hook-orm-offshore', 'sails-hook-sockets', 'offshore-memory'];
     _.each(deps, function(dep) {
-      console.log('appPath',appPath);
-      console.log('a',path.resolve(__dirname, '..', '..', 'node_modules', dep));
-      console.log('b',path.resolve(appPath, 'node_modules', dep));
       fs.ensureSymlinkSync(path.resolve(__dirname, '..', '..', 'node_modules', dep), path.resolve(appPath, 'node_modules', dep));
     });
   },
